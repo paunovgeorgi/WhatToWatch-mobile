46b9409748ff01f60766db5fbca3552c57551708
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2025-06-13 16:56:14 +0300

    MovieCard component implemented

---
+++ components/MovieCard.tsx
@@ -0,0 +1,39 @@
+import { icons } from '@/constants/icons'
+import { Link } from 'expo-router'
+import React from 'react'
+import { Image, Text, TouchableOpacity, View } from 'react-native'
+
+
+const MovieCard = ({id, poster_path, title, vote_average, release_date, original_language}: Movie ) => {
+  return (
+    <Link href={`/movies/${id}`} asChild>
+        <TouchableOpacity className='w-[30%]'>
+            <Image source={{
+                uri: poster_path
+                    ? `https://image.tmdb.org/t/p/w500${poster_path}`
+                    : 'https://placehold.co/600x400/1a1a1a/ffffff.png'  
+            }}
+            className='w-full h-52 rounded-lg'
+            resizeMode='cover'
+            />
+            <Text className='text-white text-sm font-bold mt-2' numberOfLines={1}>{title}</Text>
+
+            <View className='flex-row items-center justify-start gap-x-1'>
+                <Image source={icons.star} className='size-4'/>
+                <Text className='text-xs text-white/90 font-bold uppercase'>{Math.round(vote_average)}</Text>
+                <Text className='text-xs text-light-300'>•</Text>
+                <Text className='text-xs text-light-300 font-medium'>{release_date ? release_date.split('-')[0] : 'N/A'}</Text>
+                <Text className='text-xs text-light-300'>•</Text>
+                <Text className='text-xs text-light-300 uppercase'>{original_language}</Text>
+            </View>
+
+            {/* <View className='flex-row items-center justify-between '>
+                <Text className='text-xs text-light-300 font-medium mt-1'>{release_date ? release_date.split('-')[0] : 'N/A'}</Text>
+                <Text className='text-xs text-light-300 font-medium uppercase'>Movie</Text>
+            </View> */}
+        </TouchableOpacity>
+    </Link>
+  )
+}
+
+export default MovieCard
