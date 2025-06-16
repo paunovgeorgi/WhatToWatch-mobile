5691473a3fc8aeab1608ecf301cde9f5c0b533bc
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2025-06-16 10:38:00 +0300

    fetch movie details - function implemented

--- services/api.ts
+++ services/api.ts
@@ -13,9 +13,15 @@ export const fetchMovies = async ({ query }: {query: string}) => {
     ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
     : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
+
+    // alert(`API Key present: ${!!process.env.EXPO_PUBLIC_MOVIE_API_KEY}`);
+
     const response = await fetch(endpoint, {
         method: 'GET',
-        headers: TMDB_CONFIG.headers
+        headers: {
+          accept: 'application/json',
+          Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`
+        }
     });
 
     if (!response.ok) {
@@ -27,3 +33,27 @@ export const fetchMovies = async ({ query }: {query: string}) => {
 
     return data.results;
 }
+
+export const fetchMovieDetails = async (
+  movieId: string
+): Promise<MovieDetails> => {
+  try {
+    const response = await fetch(
+      `${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`,
+      {
+        method: "GET",
+        headers: TMDB_CONFIG.headers,
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch movie details: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching movie details:", error);
+    throw error;
+  }
+};
